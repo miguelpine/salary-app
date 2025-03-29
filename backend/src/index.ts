a367abd5bb6baccea06c4f164f29d389d706413c
Miguel Rodrigues <miguel.pinr@gmail.com>
2025-03-29 13:50:34 +0000

    0.8 :fire:

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,17 +3,26 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import salaryRoutes from './routes/salary'; // Adjust the path as needed
+const MONGO_URI = "mongodb+srv://miguelpine:<db_password>@cluster0.frsehqz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+// Use CORS middleware
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: 'GET,POST,PUT,DELETE', // Allow specific HTTP methods
+  credentials: true, // Allow cookies to be sent
+}));
+
 app.use(express.json());
 
 // Connect to MongoDB
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    await mongoose.connect(MONGO_URI!);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
@@ -30,7 +39,7 @@ app.get('/', (req, res) => {
 
 app.use('/api', salaryRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
