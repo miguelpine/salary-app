78fcbae6fd8175bb29a39f6c7b5c1e21ef94872f
Miguel Rodrigues <miguel.pinr@gmail.com>
2025-03-29 15:28:08 +0000

    :paint: Default style

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { SalaryForm } from './components/SalaryForm';
 import { SalaryList } from './components/SalaryList';
+import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="container">
       <h1>Salary Tracker</h1>
-      <SalaryForm />
-      <SalaryList />
+      <div className="card">
+        <SalaryForm />
+      </div>
+      <div className="card">
+        <SalaryList />
+      </div>
     </div>
   );
 };
