227f015d84b966917bcf6083a6359c55e9796852
Miguel Rodrigues <miguel.pinr@gmail.com>
2025-03-29 13:25:00 +0000

    0.7 :fire:

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+console.log('root:', root);
 root.render(
   <React.StrictMode>
     <App />
